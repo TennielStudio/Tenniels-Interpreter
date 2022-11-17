65a030a5071a6143562be1c32cced80c9c6fb46f
TennielTakenaka <tenniel.lynn@gmail.com>
2022-11-16 22:08:51 -0800

    Starting to build a lox interpreter
    * Building a lox interpreter in C++
    * Read command line argument
    * Open a file or image based on argument entered

--- interpreter.cpp
+++ interpreter.cpp
@@ -1,9 +1,32 @@
 #include <iostream>
+#include <string>
+#include <fstream>
+
 using namespace std;
-
-int main()
+void runFile(string path)
 {
-    cout << "hello world";
+    ifstream image;
+    image.open(path, ios::in);
+}
 
+void runPrompt()
+{
+    // todo
+}
+
+int main(int argc, char *argv[])
+{
+    if (argc > 2)
+    {
+        cout << "Usage: jlox [script]" << endl;
+    }
+    else if (argc == 2)
+    {
+        runFile(argv[1]);
+    }
+    else
+    {
+        runPrompt();
+    }
     return 0;
 }
